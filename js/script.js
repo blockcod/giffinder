c102954aac4427ea01e4f1879843052579d1d3bc
Josue <jescamilla@aclanyc.com>
2018-03-19 22:24:37 +0000

    randomizer

--- js/script.js
+++ js/script.js
@@ -4,7 +4,37 @@
 
 $(document).ready(function(){
   
+  function giphyURLWithSearchTerm(searchTerm) {
+    // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+       $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+         $('.result').html("<img src="+ response.data[0].images.original.url + ">");
+      },   
+    }); 
+}
+$(".custom").click(function(){
+     var searchterm = $("#main").val(); 
+           console.log(searchterm);
+    giphyURLWithSearchTerm(searchterm);
+});
   
+  function giphyRandom(){
+   $.ajax({
+      url: "https://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response){
+          $('.result').html("<img src="+ response.data.images.original.url + ">");
+          console.log(response)
+  },
+   });
+  }
   
+  $(".rng").click(function(){
+     giphyRandom();
+     
+});
   
 });
